0fc80fe458abc1ba558ad9f7b95a56724ebe7122
Dennis Ruiter <dennis.ruiter@thewebmen.com>
2023-07-06 13:24:18 +0200

    Fixed errors during build step

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,7 @@
-module.exports = require('@silverstripe/eslint-config/.eslintrc');
+const config = require('@silverstripe/eslint-config/.eslintrc');
+
+config.rules['import/no-unresolved'] = 'off';
+config.rules['import/extensions'] = 'off';
+config.rules['react/jsx-boolean-value'] = 'always';
+
+module.exports = config;
