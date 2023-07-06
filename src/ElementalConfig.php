40c65c5f61e9b767080f4ba2050ae5b8c729f79d
Dennis Prins <dennis@wedevelop.nl>
2023-07-06 14:34:27 +0200

    Revert to SS4

--- src/ElementalConfig.php
+++ src/ElementalConfig.php
@@ -3,57 +3,34 @@
 namespace WeDevelop\ElementalGrid;
 
 use SilverStripe\Core\Config\Config;
+use WeDevelop\ElementalGrid\CSSFramework\BulmaCSSFramework;
 
 final class ElementalConfig
 {
-    /***
-     * @var bool
-     */
-    private static $enable_custom_title_classes;
+    private static bool $enable_custom_title_classes = true;
 
-    /***
-     * @var string
-     */
-    private static $default_viewport;
+    private static string $default_viewport = 'MD';
 
-    /***
-     * @var string
-     */
-    private static $default_title_tag;
+    private static string $default_title_tag = 'h2';
 
-    /***
-     * @var string
-     */
-    private static $css_framework;
+    private static string $css_framework = BulmaCSSFramework::class;
 
-    /**
-     * @return bool
-     */
-    public static function getEnableCustomTitleClasses()
+    public static function getEnableCustomTitleClasses(): bool
     {
         return Config::inst()->get(__CLASS__, 'enable_custom_title_classes');
     }
 
-    /**
-     * @return string
-     */
-    public static function getDefaultViewport()
+    public static function getDefaultViewport(): string
     {
         return Config::inst()->get(__CLASS__, 'default_viewport');
     }
 
-    /**
-     * @return string
-     */
-    public static function getDefaultTitleTag()
+    public static function getDefaultTitleTag(): string
     {
         return Config::inst()->get(__CLASS__, 'default_title_tag');
     }
 
-    /**
-     * @return string
-     */
-    public static function getCSSFrameworkName()
+    public static function getCSSFrameworkName(): string
     {
         return Config::inst()->get(__CLASS__, 'css_framework');
     }
