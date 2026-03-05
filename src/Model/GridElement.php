307f18723f4fc597417a09a16f36e94e633902a4
Erik Frèrejean <erik@wedevelop.nl>
2026-03-05 13:34:13 +0100

    fix: remove unused DBField import from GridElement

--- src/Model/GridElement.php
+++ src/Model/GridElement.php
@@ -6,7 +6,6 @@ namespace WeDevelop\Grid\Model;
 
 use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\ORM\DataObject;
-use SilverStripe\ORM\FieldType\DBField;
 use SilverStripe\Security\Member;
 use SilverStripe\Security\Permission;
 use SilverStripe\Versioned\Versioned;
